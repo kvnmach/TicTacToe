70a10e6f5d416cf84ef9514489e2b2a8c8192951
kvnmach <kvnmach@gmail.com>
2016-03-15 15:06:41 -0500

    Game works now.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Tic Tac Toe");
 
-            char currentPlayer = 'X';
+            var currentPlayer = 'X';
 
 
-            var board = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
+            var board = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
 
             while (true)
             {
-
                 Console.WriteLine(" {0} | {1} | {2}", board[0], board[1], board[2]);
                 Console.WriteLine("---------------");
                 Console.WriteLine(" {0} | {1} | {2}", board[3], board[4], board[5]);
@@ -28,91 +23,91 @@ namespace TicTacToe
 
                 Console.WriteLine($"Player {currentPlayer}, please enter a move");
 
-                string userInput = Console.ReadLine();
+                var userInput = Console.ReadLine();
                 var move = int.Parse(userInput);
 
-                if (currentPlayer == 'X')
-                { currentPlayer = 'O'; }
-                else
-                { currentPlayer = 'X'; }
+
+                board[move] = currentPlayer;
 
 
 
-                if (move <= 0 || move <= 8)
+                if (board[0] == board[1] && board[1] == board[2])
                 {
-                    board[move] = currentPlayer;
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+                if (board[3] == board[4] && board[4] == board[5])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+                if (board[6] == board[7] && board[7] == board[8])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+
+                if (board[0] == board[3] && board[3] == board[6])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+                if (board[1] == board[4] && board[4] == board[7])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+                if (board[2] == board[5] && board[5] == board[8])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+
+                if (board[0] == board[4] && board[4] == board[8])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+                if (board[2] == board[4] && board[4] == board[6])
+                {
+                    Console.WriteLine("You won");
+                    Console.ReadLine();
+                    break;
+                }
+
+
+                var isBoardFull = true;
+                foreach (var spot in board)
+                {
+                    if (spot != 'X' || spot != 'O')
+                    {
+                        isBoardFull = false;
+                        break;
+                    }
+                }
+
+                if (isBoardFull)
+                {
+                    Console.Write("You Win!");
+                    break;
+                }
+
+
+                //switch turn
+                if (currentPlayer == 'X')
+                {
+                    currentPlayer = 'O';
                 }
                 else
                 {
-
-                    continue;
-                    if (move <= 0 || move <= 8)
-                    {
-                        board[move] = currentPlayer;
-                    }
-                    else
-                    {
-
-                        continue;
-                    }
-
-
-                    if (board[0] == board[1] && board[1] == board[2])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-                    if (board[3] == board[4] && board[4] == board[5])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-                    if (board[6] == board[7] && board[7] == board[8])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-
-                    if (board[0] == board[3] && board[3] == board[6])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-                    if (board[1] == board[4] && board[4] == board[7])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-                    if (board[2] == board[5] && board[5] == board[8])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-
-                    if (board[0] == board[4] && board[4] == board[8])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-                    if (board[2] == board[4] && board[4] == board[6])
-                    {
-                        Console.WriteLine("You won");
-                        Console.ReadLine();
-                    }
-
-                    bool isBoardFull = true;
-                    foreach (var spot in board)
-                    {
-
-
-                        { isBoardFull = false; break; }
-                        if (isBoardFull) { Console.Write("You Win!"); };
-
-
-
-
-
-                    }
+                    currentPlayer = 'X';
                 }
             }
         }
